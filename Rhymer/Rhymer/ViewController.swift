d80c6b677d8bf296c41078d65bddf2cc6b34ffe4
Arya Gharib <aryakhan81@icloud.com>
2018-07-16 14:06:41 -0700

    Added Xcode file

---
+++ Rhymer/Rhymer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rhymer
+//
+//  Created by Arya Gharib on 7/16/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
