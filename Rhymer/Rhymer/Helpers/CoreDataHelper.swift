1c17fcb83e12367ef722cb25ba8c86cb6de137a9
Arya Gharib <aryakhan81@icloud.com>
2018-07-17 10:46:21 -0700

    Improved CoreDataHelper

--- Rhymer/Rhymer/Helpers/CoreDataHelper.swift
+++ Rhymer/Rhymer/Helpers/CoreDataHelper.swift
@@ -12,6 +12,14 @@ import CoreData
 
 struct CoreDataHelper {
     static let context: NSManagedObjectContext = {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError("error") }
-    }
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+        
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+        
+        return context
+    }()
+    
 }
