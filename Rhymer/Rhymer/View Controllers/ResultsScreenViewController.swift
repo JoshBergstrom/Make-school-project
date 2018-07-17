39af64f1ee3346785532ee106a0996e3cbe8f6de
josh Bergstrom <joshBergstrom463@gmail.com>
2018-07-17 16:54:46 -0700

    Removed the empty tab bar on the home screen

---
+++ Rhymer/Rhymer/View Controllers/ResultsScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsScreenViewController.swift
+//  Rhymer
+//
+//  Created by Josh Bergstrom on 7/17/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
