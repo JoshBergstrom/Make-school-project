b887a34deb8d3896862a784150ee54fa3ef46934
Anna Grace <agrace@exeter.edu>
2018-07-18 09:52:15 -0700

    Tab Bar View Controller

---
+++ Rhymer/Rhymer/View Controllers/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Rhymer
+//
+//  Created by Anna Grace on 7/18/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
