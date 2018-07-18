eb418279c94f449203d59af5ca024616f8764a37
Anna Grace <agrace@exeter.edu>
2018-07-18 14:48:18 -0700

    Fuck you thats why

---
+++ Rhymer/Rhymer/View Controllers/CustomTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTabViewController.swift
+//  Rhymer
+//
+//  Created by Anna Grace on 7/18/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTabViewController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 1;
+    }
+}
