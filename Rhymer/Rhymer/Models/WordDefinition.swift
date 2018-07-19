bfd6835521c268298c1234b2299f8ce9df61c094
MelissaFlores2002 <41021321+MelissaFlores2002@users.noreply.github.com>
2018-07-19 16:05:07 -0700

    Revert "Merge pull request #15 from JoshBergstrom/storyboard/melissa"
    This reverts commit e1559bcd5847f164f479b6c3d27cddbe1043b5ac, reversing
    changes made to e8e352101c3249b5c8433247906534422b69e9b2.

---
+++ Rhymer/Rhymer/Models/WordDefinition.swift
@@ -0,0 +1,53 @@
+//
+//  WordDefinition.swift
+//  Rhymer
+//
+//  Created by Arya Gharib on 7/19/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+
+struct WordDefinition {
+    let appId = "bbdc0eb3"
+    let appKey = "599ae31c8e901789d51a534320d47959"
+    let language = "en"
+    let word: String
+    let word_id: String
+    let url: URL
+    var request: URLRequest
+    
+    init(wordToDefine word: String, label: UILabel) {
+        self.word = word
+        self.word_id = word.lowercased()
+        self.url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)")!
+        self.request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+        let session = URLSession.shared
+        _ = session.dataTask(with: request, completionHandler: { data, response, error in
+            if let response = response,
+                let data = data,
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+                if let dictionary = jsonData as? [String: Any]{
+                    if let def = dictionary["results"] as? [String: Any] {
+                        if let nestDef = def["lexicalEntries"] as? [String: Any] {
+                            if let nestDef2 = nestDef["entries"] as? [String: Any] {
+                                if let actualDef = nestDef2["entries"] as? String {
+                                    label.text = "Definition: \(actualDef)"
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                print(error!)
+                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue)!)
+            }
+        }).resume()
+    }
+}
