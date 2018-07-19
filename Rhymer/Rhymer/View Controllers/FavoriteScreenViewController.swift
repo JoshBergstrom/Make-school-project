7d6501748f7f79c457e474d5b10caa0575a179bb
MelissaFlores2002 <41021321+MelissaFlores2002@users.noreply.github.com>
2018-07-19 10:11:50 -0700

    New View Controllers

---
+++ Rhymer/Rhymer/View Controllers/FavoriteScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteScreenViewController.swift
+//  Rhymer
+//
+//  Created by Melissa Flores on 7/19/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoriteScreenViewController : UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Rhymer/Rhymer/View Controllers/NotesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotesViewController.swift
+//  Rhymer
+//
+//  Created by Melissa Flores on 7/19/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotesViewController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+}
